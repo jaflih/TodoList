85e10ac69d9ff5b8ef4cf155ddfe0a00778187b1
jihaneh <jijih@hotmail.fr>
2022-01-12 12:13:47 +0100

    Add fontawesome via npm

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import './style.css';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
+
 import Task from './modules/task.js';
 import DisplayManager from './modules/display_manager.js';
 
