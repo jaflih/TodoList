fb81938301bcb261be46606a25ddabfd3d205f3a
jihaneh <jijih@hotmail.fr>
2022-01-17 09:07:09 +0100

    Eliminate Circular Dependencies

--- src/index.js
+++ src/index.js
@@ -1,83 +1,102 @@
 /* eslint-disable no-use-before-define */
 import './style.css';
 import TasksManager from './modules/tasks_manager.js';
-import DisplayManager from './modules/display_manager.js';
-import { selector, selectorAll } from './modules/tools.js';
+import { selector } from './modules/tools.js';
 import DragAndDrop from './modules/drag_and_drop.js';
 
 const input = selector('input');
 const manager = new TasksManager();
 
-const updateTask = (event, index, focus = false) => {
+const reset = (element) => {
+  element.innerHTML = '';
+};
+
+const displayTask = (parent, task) => {
+  const taskDiv = createHtml(parent, 'div', 'task');
+  taskDiv.dataset.id = task.index;
+  taskDiv.id = `task_${task.index}`;
+  taskDiv.setAttribute('draggable', 'true');
+  taskDiv.setAttribute('data-effectallowed', 'move');
+  taskDiv.setAttribute('data-position', task.index - 1);
+  taskDiv.setAttribute('data-id', task.index);
+
+  taskDiv.addEventListener('dragstart', DragAndDrop.dragStartEvent);
+  taskDiv.addEventListener('dragend', DragAndDrop.dragEndEvent);
+
+  const taskInput = createHtml(taskDiv, 'div', 'task_input');
+  taskInput.addEventListener('keyup', (event) => {
+    updateTask(event, task.index);
+  });
+
+  const input = createHtml(taskInput, 'input');
+  input.setAttribute('type', 'checkbox');
+  input.setAttribute('class', `task_${task.index}_checkbox checkbox_task`);
+  input.id = `checkbox_task_${task.index}`;
+  input.dataset.id = task.index;
+  input.addEventListener('change', () => {
+    updateStatus(task.index);
+  });
+  input.addEventListener('click', () => {
+    deleteTask(task.index);
+  });
+
+  const input2 = createHtml(taskInput, 'input');
+  input2.setAttribute('type', 'text');
+  input2.setAttribute('class', `task_${task.index} input_task`);
+  input2.dataset.id = task.index;
+  input2.value = task.description;
+  input2.id = `input_task_${task.index}`;
+  input2.addEventListener('keyup', (event) => {
+    updateTask(event, task.index);
+  });
+  input2.addEventListener('focusout', (event) => {
+    updateTask(event, task.index, true);
+  });
+
+  const i = createHtml(taskDiv, 'i', 'fas fa-ellipsis-v');
+  i.setAttribute('class', 'fas fa-trash');
+  i.id = `task_${task.index}`;
+  i.dataset.id = task.index;
+  i.addEventListener('click', () => {
+    deleteTask(task.index);
+  });
+};
+
+const createHtml = (parent, tag, className, content = null) => {
+  const element = document.createElement(tag);
+  element.className = className;
+  element.innerHTML = content;
+  parent.appendChild(element);
+  return element;
+};
+
+export const updateTask = (event, index, focus = false) => {
   if (event.key === 'Enter' || focus) {
     manager.updateTask(index, selector(`.task_${index}`).value, selector(`.task_${index}_checkbox`).checked);
     selector('.add_task input').focus();
   }
 };
 
-const updateStatus = (index) => {
+export const updateStatus = (index) => {
   manager.updateStatus(index, selector(`.task_${index}_checkbox`).checked);
   selector(`.task[data-id='${index}'] .input_task`).classList.toggle('completed');
 };
 
 const display = () => {
-  DisplayManager.reset(selector('.tasks'));
-
-  manager.getTasks().forEach((task) => DisplayManager.displayTask(selector('.tasks'), task));
-
-  selectorAll('.fa-trash').forEach((e) => e.addEventListener('click', () => {
-    deleteTask(e.dataset.id);
-  }));
-
-  selectorAll('.input_task').forEach((e) => e.addEventListener('keyup', (event) => {
-    updateTask(event, e.dataset.id);
-  }));
-
-  selectorAll('.checkbox_task').forEach((e) => e.addEventListener('change', () => {
-    updateStatus(e.dataset.id);
-  }));
+  reset(selector('.tasks'));
+  manager.getTasks().forEach((task) => displayTask(selector('.tasks'), task));
 };
 
-const deleteTask = (index) => {
+export const deleteTask = (index) => {
   manager.deleteTask(index);
   display();
-
-  selectorAll('.input_task').forEach((e) => e.addEventListener('keyup', (event) => {
-    updateTask(event, e.dataset.id);
-  }));
-
-  selectorAll('.input_task').forEach((e) => e.addEventListener('focusout', (event) => {
-    updateTask(event, e.dataset.id, true);
-  }));
 };
 
 const createTask = () => {
   const task = manager.addTask(input.value);
-  DisplayManager.displayTask(selector('.tasks'), task);
+  displayTask(selector('.tasks'), task);
   input.value = '';
-
-  selector(`#task_${task.index}`).addEventListener('click', () => {
-    deleteTask(task.index);
-  });
-
-  selector(`#input_task_${task.index}`).addEventListener('keyup', (event) => {
-    updateTask(event, task.index);
-  });
-
-  selector(`#input_task_${task.index}`).addEventListener('focusout', (event) => {
-    updateTask(event, task.index, true);
-  });
-
-  selector(`#checkbox_task_${task.index}`).addEventListener('change', () => {
-    updateStatus(task.index);
-  });
-
   selector(`#input_task_${task.index}`).focus();
-
-  selectorAll('.task').forEach((e) => {
-    e.addEventListener('dragstart', DragAndDrop.dragStartEvent);
-    e.addEventListener('dragend', DragAndDrop.dragEndEvent);
-  });
 };
 
 selector('.footer').addEventListener('click', () => {
@@ -94,13 +113,6 @@ input.addEventListener('keyup', ({ key }) => {
 selector('.fa-plus').addEventListener('click', () => createTask());
 
 display();
-
-//
-
-selectorAll('.task').forEach((e) => {
-  e.addEventListener('dragstart', DragAndDrop.dragStartEvent);
-  e.addEventListener('dragend', DragAndDrop.dragEndEvent);
-});
 
 const depot = document.querySelector('.depot');
 depot.addEventListener('dragenter', DragAndDrop.dragEnterEvent);
